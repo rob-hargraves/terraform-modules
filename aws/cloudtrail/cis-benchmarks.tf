6fcb9a0c4bbb94d6e17db8f4177ac6e48e60cb35
Rob Hargraves <rob.hargraves@helplightning.com>
2020-11-30 11:08:53 -0600

    Cloudtrail: Added unauthorized_api_calls metric filter and alarm

---
+++ aws/cloudtrail/cis-benchmarks.tf
@@ -0,0 +1,33 @@
+resource "aws_sns_topic" "cis_benchmarks" {
+  count = "${length(var.cis_benchmark_alerts) != 0 ? 1 : 0}"
+
+  name = "cis-benchmarks"
+}
+
+resource "aws_cloudwatch_log_metric_filter" "unauthorized_api_calls" {
+  count = "${contains(var.cis_benchmark_alerts, "unauthorized_api_calls") ? 1 : 0}"
+
+  log_group_name = "${aws_cloudwatch_log_group.cloudtrail.name}"
+  name           = "Unauthorized Api Calls"
+  pattern        = "{($.errorCode=\"*UnauthorizedOperation\") || ($.errorCode=\"AccessDenied*\")}"
+
+  metric_transformation {
+    name      = "UnauthorizedApiCalls"
+    namespace = "LogMetrics"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "unauthorized_api_calls" {
+  count = "${contains(var.cis_benchmark_alerts, "unauthorized_api_calls") ? 1 : 0}"
+
+  alarm_description   = "CIS Benchmark: Unauthorized Api Calls"
+  alarm_name          = "${var.account_name}-unauthorized-api-calls"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "${aws_cloudwatch_log_metric_filter.unauthorized_api_calls.metric_transformation["name"]}"
+  namespace           = "${aws_cloudwatch_log_metric_filter.unauthorized_api_calls.metric_transformation["namespace"]}"
+  period              = "300"
+  statistic           = "Sum"
+  threshold           = "1"
+}
