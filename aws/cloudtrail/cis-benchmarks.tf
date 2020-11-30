ded38cd42e8c9f38ef12ba2d5151d2f3b6ddc130
Rob Hargraves <rob.hargraves@helplightning.com>
2020-11-30 14:58:47 -0600

    Cloudtrail: Added cloudtrail_changes benchmark

--- aws/cloudtrail/cis-benchmarks.tf
+++ aws/cloudtrail/cis-benchmarks.tf
@@ -123,3 +123,33 @@ resource "aws_cloudwatch_metric_alarm" "iam_policy_changes" {
   threshold           = "1"
   treat_missing_data  = "notBreaching"
 }
+
+resource "aws_cloudwatch_log_metric_filter" "cloudtrail_changes" {
+  count = "${contains(var.cis_benchmark_alerts, "cloudtrail_changes") ? 1 : 0}"
+
+  log_group_name = "${aws_cloudwatch_log_group.cloudtrail.name}"
+  name           = "CloudTrail Changes"
+  pattern        = "{($.eventName=CreateTrail) || ($.eventName=UpdateTrail) || ($.eventName=DeleteTrail) || ($.eventName=StartLogging) || ($.eventName=StopLogging)}"
+
+  metric_transformation {
+    name      = "CloudTrailChanges"
+    namespace = "LogMetrics"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "cloudtrail_changes" {
+  count = "${contains(var.cis_benchmark_alerts, "cloudtrail_changes") ? 1 : 0}"
+
+  alarm_actions       = ["${aws_sns_topic.cis_benchmarks.arn}"]
+  alarm_description   = "CIS Benchmark: CloudTrail Changes"
+  alarm_name          = "${var.account_name}-cloudtrail-changes"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "${aws_cloudwatch_log_metric_filter.cloudtrail_changes.metric_transformation.0.name}"
+  namespace           = "${aws_cloudwatch_log_metric_filter.cloudtrail_changes.metric_transformation.0.namespace}"
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "1"
+  treat_missing_data  = "notBreaching"
+}
