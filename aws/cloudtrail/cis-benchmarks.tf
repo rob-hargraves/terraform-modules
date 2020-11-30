a523824bdf98ac78f8ff30ae864bfca75ed4f530
Rob Hargraves <rob.hargraves@helplightning.com>
2020-11-30 15:10:44 -0600

    Cloudtrail: Added disable_or_delete_cmk benchmark

--- aws/cloudtrail/cis-benchmarks.tf
+++ aws/cloudtrail/cis-benchmarks.tf
@@ -183,3 +183,33 @@ resource "aws_cloudwatch_metric_alarm" "console_authentication_failure" {
   threshold           = "1"
   treat_missing_data  = "notBreaching"
 }
+
+resource "aws_cloudwatch_log_metric_filter" "disable_or_delete_cmk" {
+  count = "${contains(var.cis_benchmark_alerts, "disable_or_delete_cmk") ? 1 : 0}"
+
+  log_group_name = "${aws_cloudwatch_log_group.cloudtrail.name}"
+  name           = "Disable Or Delete CMK"
+  pattern        = "{($.eventSource=kms.amazonaws.com) && (($.eventName=DisableKey) || ($.eventName=ScheduleKeyDeletion))}"
+
+  metric_transformation {
+    name      = "DisableOrDeleteCMK"
+    namespace = "LogMetrics"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "disable_or_delete_cmk" {
+  count = "${contains(var.cis_benchmark_alerts, "disable_or_delete_cmk") ? 1 : 0}"
+
+  alarm_actions       = ["${aws_sns_topic.cis_benchmarks.arn}"]
+  alarm_description   = "CIS Benchmark: Disable Or Delete CMK"
+  alarm_name          = "${var.account_name}-disable-or-delete-cmk"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "${aws_cloudwatch_log_metric_filter.disable_or_delete_cmk.metric_transformation.0.name}"
+  namespace           = "${aws_cloudwatch_log_metric_filter.disable_or_delete_cmk.metric_transformation.0.namespace}"
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "1"
+  treat_missing_data  = "notBreaching"
+}
