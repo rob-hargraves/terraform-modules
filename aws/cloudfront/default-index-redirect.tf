03d46dbbd8d3506a435caa69bc285b959c71cea8
Rob Hargraves <rob.hargraves@helplightning.com>
2023-06-15 12:35:46 -0500

    aws/cloudfront: Update nodejs in redirector lambda function to the latest 18.x

--- aws/cloudfront/default-index-redirect.tf
+++ aws/cloudfront/default-index-redirect.tf
@@ -62,7 +62,7 @@ resource "aws_lambda_function" "redirector" {
   provider         = aws.lambda_edge_region
   publish          = true
   role             = aws_iam_role.redirector.arn
-  runtime          = "nodejs12.x"
+  runtime          = "nodejs18.x"
   source_code_hash = filebase64sha256("${path.module}/default-index-redirect/function.zip")
   tags             = local.tags
 }
